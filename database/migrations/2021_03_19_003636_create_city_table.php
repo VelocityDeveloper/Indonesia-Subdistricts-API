15edbe3fce1ef90ad21c54fb5104a6268aab1584
aadiityaak <aadiityaak@gmail.com>
2021-03-19 08:09:15 +0700

    Add city table

---
+++ database/migrations/2021_03_19_003636_create_city_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCityTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('city', function (Blueprint $table) {
+            $table->integer('city_id');
+            $table->integer('province_id');
+            $table->string('province');
+            $table->string('type');
+            $table->string('city_name');
+            $table->integer('postal_code');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('city');
+    }
+}
